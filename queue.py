9f49faf5cba70aa548553f8d1aec467086ea0361
ArpanMahatra1999 <arpanmahatra1999ad@gmail.com>
2022-09-10 14:13:38 +0545

    Queue DS created

---
+++ queue.py
@@ -0,0 +1,25 @@
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Enqueue
+    def enqueue(self, element):
+        self.queue.append(item)
+
+    # Dequeue
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return "Queue is Empty"
+        return self.queue.pop(0)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+print(q.queue)
+q.dequeue()
+print(q.queue)
